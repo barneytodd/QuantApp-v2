2c2eef17e28f7809ad9114a6e4427f7d83605c41
Barney Todd <barneytodd27@gmail.com>
2026-01-13 18:04:46 +0000

    feat(api/data): add adapter to transform data validation results into the correct format to send to frontend

---
+++ backend/app/api/routes/data/adapters/validation.py
@@ -0,0 +1,31 @@
+from typing import List
+from app.data_validation.models import ValidationResult
+from app.schemas.prices.validation import ValidationResultRow, ValidationIssueRow
+
+def adapt_validation_result(results: List[ValidationResult]) -> List[ValidationResultRow]:
+    """
+    Convert domain ValidationResult objects into API-ready Pydantic models.
+    """
+    response = []
+    for r in results:
+        issues = [
+            ValidationIssueRow(
+                code=i.code,
+                description=i.description,
+                affected_dates=i.affected_dates,
+                count=i.count
+            )
+            for i in r.issues
+        ]
+        response.append(
+            ValidationResultRow(
+                symbol=r.symbol,
+                start=r.start,
+                end=r.end,
+                coverage=r.coverage,
+                first_date=r.first_date,
+                last_date=r.last_date,
+                issues=issues
+            )
+        )
+    return response
